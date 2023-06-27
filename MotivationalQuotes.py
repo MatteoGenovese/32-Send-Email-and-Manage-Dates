05eb190b956f12e24763e2d2f728e7fbd4b86962
matteo <mgenovese@doxee.com>
2023-06-27 18:54:20 +0200

    feature: send motivational quotes on Tuesday

---
+++ MotivationalQuotes.py
@@ -0,0 +1,27 @@
+import datetime
+import smtplib
+import password
+import random
+
+now = datetime.datetime.now()
+day_of_week = now.weekday()
+myEmail = "matteo.genovese.91@gmail.com"
+
+
+def sendquote(quoteToSend):
+    # search your smtp information by google it
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        # create transport layer security, it protect the connection at our e-mail server
+        connection.starttls()
+        connection.login(user=myEmail, password=password.myPassword)
+        connection.sendmail(from_addr=myEmail, to_addrs="matteo.genovese@icloud.com",
+                            msg=f"Subject:Quote\n\n{quoteToSend}")
+        connection.close()
+
+
+# On tuesday, send quote to a provided email
+if day_of_week == 1:
+    with open("quotes.txt") as quotes_file:
+        quotesFile = quotes_file.readlines()
+        quote = random.choice(quotesFile)
+        sendquote(quote)
